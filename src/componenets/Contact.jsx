65c0a1def92d97841f33c8e79af79adfd2581487
priyannnnnn <supriyanto031003@gmail.com>
2025-04-29 22:57:50 +0700

    portofolio

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -6,9 +6,9 @@ const Contact = () => {
       <div className="max-w-4xl mx-auto px-4 text-center">
         <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
         <p className="text-gray-700 mb-4">Feel free to reach out for collaborations or just a friendly hello!</p>
-        <p className="text-gray-700">Email: your.email@example.com</p>
-        <p className="text-gray-700">LinkedIn: linkedin.com/in/yourprofile</p>
-        <p className="text-gray-700">GitHub: github.com/yourusername</p>
+        <p className="text-gray-700">Email: supriyanto031003@gmail.com</p>
+        <p className="text-gray-700">LinkedIn: https://www.linkedin.com/in/supri-yanto-62367024b/</p>
+        <p className="text-gray-700">GitHub: https://github.com/priyannnnnn</p>
       </div>
     </section>
   );
